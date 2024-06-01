bd196214894e15d1edc57fbe87352c12470da668
joe-at-startupmedia <joseph.persie@startupmedia.com>
2024-06-01 04:04:43 +0000

    Apply ownership to both files

--- mem/wrapper.go
+++ mem/wrapper.go
@@ -15,15 +15,15 @@ type QueueConfig struct {
 	Mode       int
 }
 
-func (qc *QueueConfig) GetFile() string {
-	return qc.BasePath + qc.Name
+func (qc *QueueConfig) GetFile(configName string) string {
+	if len(configName) > 0 {
+		return qc.BasePath + configName
+	} else {
+		return qc.BasePath + qc.Name
+	}
 }
 
 func NewResponderWithOwnership(config *QueueConfig, owner *xipc.Ownership, postfix string) (*shmemipc.IpcResponder, error) {
-
-	if len(postfix) > 0 {
-		config.Name = fmt.Sprintf("%s_%s", config.Name, postfix)
-	}
 
 	if config.Mode == 0 {
 		if owner != nil && owner.IsValid() {
@@ -34,15 +34,36 @@ func NewResponderWithOwnership(config *QueueConfig, owner *xipc.Ownership, postf
 		}
 	}
 
-	responder := shmemipc.NewResponder(config.GetFile(), config.MaxMsgSize)
+	responder := shmemipc.NewResponder(config.GetFile(""), config.MaxMsgSize)
 	if err := responder.GetError(); err != nil {
-		return nil, errors.New(fmt.Sprintf("Could not create message queue %s: %-v", config.GetFile(), err))
+		return nil, errors.New(fmt.Sprintf("Could not create message queue %s: %-v", config.GetFile(""), err))
 	}
 
-	err := owner.ApplyPermissions(config.GetFile(), config.Mode)
+	err := ApplyOwnership(config, owner, "_rqst")
 	if err != nil {
-		return responder, errors.New(fmt.Sprintf("Could not apply permissions %s: %-v", config.GetFile(), err))
+		return nil, err
+	}
+
+	err = ApplyOwnership(config, owner, "_resp")
+	if err != nil {
+		return nil, err
 	}
 
 	return responder, nil
+}
+
+func ApplyOwnership(config *QueueConfig, owner *xipc.Ownership, postfix string) error {
+
+	var configName string //make a copy so we don't modify the reference
+
+	if len(postfix) > 0 {
+		configName = fmt.Sprintf("%s_%s", config.Name, postfix)
+	}
+
+	err := owner.ApplyPermissions(config.GetFile(configName), config.Mode)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Could not apply permissions %s: %-v", config.GetFile(configName), err))
+	}
+
+	return nil
 }
